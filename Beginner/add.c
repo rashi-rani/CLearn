7dcd77e73a341ff79aa04aac2faab6a9d0ab77b4
rashi-rani <rashi28rani@gmail.com>
2019-12-05 22:55:50 +0530

    Program to add two numbers

---
+++ Beginner/add.c
@@ -0,0 +1,10 @@
+//A program to add two numbers
+
+#include<stdio.h>
+void main(){
+    int num1,num2,sum=0;
+    printf("Enter both numbers\n");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("The sum is %d \n",sum);
+}
