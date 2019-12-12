d6399399f5a508c704424b9314101fd51707bf0f
rashi-rani <rashi28rani@gmail.com>
2019-12-12 22:31:33 +0530

    Number Triangle⭐️

---
+++ Beginner/numberTriangle.c
@@ -0,0 +1,18 @@
+//write a program to print the followiing pattern
+/* 1
+   12 
+   123 
+   1234 
+   12345   */
+
+   #include<stdio.h>
+   void main(){
+       int i,j;
+       for(i=1;i<=5;i++){
+           printf("\n");
+           for(j=1;j<=i;j++){
+               printf("%d",i);
+           }           
+       }
+   }
+
