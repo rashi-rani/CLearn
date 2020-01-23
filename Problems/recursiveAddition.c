8a64b652f0644306ccba9941ec28629f66e6b9e3
rashi-rani <rashi28rani@gmail.com>
2020-01-23 23:15:09 +0530

    To add n numbers using recursive function

--- Problems/recursiveAddition.c
+++ Problems/recursiveAddition.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
+int recursiveAdder(int number);
 int main() {
     int number, sum = 0;
     printf("Enter the number:\n");
     scanf("%d",&number);
     sum = recursiveAdder(number);
+    printf("The sum is %d",sum);
+    return 0;
 }
 
-int recusiveAdder(int number) {
-    int sum = 0;
-    if( number == 1 ) {
-        sum += number;
+int recursiveAdder(int number) {
+    
+    if( number == 0 ) {
+     return number;
     } else {
-        sum += recursiveAdder(number - 1);
+        return number+ recursiveAdder(number-1);
     }
-    return sum;
+    
 }
