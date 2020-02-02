7b89d61bcb94b1e6e53bfc36d29cad2e9a944df6
rashi-rani <rashi28rani@gmail.com>
2020-02-02 18:51:11 +0530

    Product of pythogorean triplet a,b,c such that a+b+c=1000

--- Problems/New/pythogoreanTriplet.c
+++ Problems/New/pythogoreanTriplet.c
@@ -2,3 +2,32 @@
 a^2 + b^2 = c^2
 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc such that a + b + c = n. */
+
+#include<stdio.h>
+int main(){
+    int a,b,c,sum=0,product=1;
+    for(a=1;a<1000;a++){
+        for(b=2;b<1000;b++){
+            for(c=3;c<1000;c++){
+                if(a*a+b*b==c*c){
+                    sum= a+b+c;
+                    if (sum==1000){
+                        printf("The required numbers are %d,%d and %d\n",a,b,c);
+                        product=a*b*c;
+                        printf("The product of abc such that a+b+c=1000 is %d\n",product);
+
+                    }
+
+
+                }
+            }
+        }
+
+    }
+
+}
+
+/*Answer : a=200
+           b=375
+           c=425
+           Product=31875000 */
